1b52d9f86cbaf2f77b60cba0a08bb09321671bf6
kevinriverosguia <riverosk885@gmail.com>
2018-10-26 00:35:28 -0500

    Initial Commit

---
+++ LeaCouch/Network/NewApiTutor.swift
@@ -0,0 +1,14 @@
+//
+//  NewApiTutor.swift
+//  LeaCouch
+//
+//  Created by ALEXIS-PC on 10/24/18.
+//  Copyright © 2018 UPC. All rights reserved.
+//
+
+import Foundation
+
+class NewApiTutor{
+    static let baseUrl = "https://socket.tutorcruncher.com/9c79f14df986a1ec693c"
+    static let gamesUrl = baseUrl + "/contractors"
+}
