0f75f22b9cc521f9b58fa0eb8276f8f4695f9e11
Jerber Valentin <u201521654@upc.edu.pe>
2018-11-15 22:57:02 -0500

    other

--- LeaCouch/ViewController/MasterSearchViewController.swift
+++ LeaCouch/ViewController/MasterSearchViewController.swift
@@ -8,49 +8,79 @@
 
 import UIKit
 
-class MasterSearchViewController: UIViewController {
+class MasterSearchViewController: UIViewController, UITableViewDataSource {
+    
     @IBOutlet weak var segmentedControl: UISegmentedControl!
     @IBOutlet weak var coachesView: UIView!
     @IBOutlet weak var publicationsView: UIView!
+    //to search
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var coachTable: UITableView!
+    
+    var coachesArray = [Coachesx]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //setupView()
+        setUpCoaches()
     }
     
-    
+    // tochange
     @IBAction func changeSegmentedControlViews(_ sender: UISegmentedControl) {
         if sender.selectedSegmentIndex == 0 {
             coachesView.alpha = 1
             publicationsView.alpha = 0
+            
         } else {
             coachesView.alpha = 0
             publicationsView.alpha = 1
         }
     }
     
-    
-    //view methods
-    /*private func setupView() {
-        setupSegmentedContro()
+    // to search
+    private func setUpCoaches() {
+        coachesArray.append(Coachesx(name: "Maria enriquez", correo: "kevinEnriquez@correo.com", speciality: .engineer, photo: "https://www.b-plast2000.com/uploads/pics/maria_01.jpg"))
+        coachesArray.append(Coachesx(name: "Roberto Marc", correo: "RobertoEnriquez@correo.com", speciality: .noEngineer, photo: "https://d26oc3sg82pgk3.cloudfront.net/files/media/edit/image/82/square_thumb%402x.jpg"))
+        coachesArray.append(Coachesx(name: "Richard Juarex", correo: "RichardEnriquez@correo.com", speciality: .engineer, photo: "https://www.ktbyte.com/resources/dist/assets/images/people/richard_zhou.jpg"))
+        coachesArray.append(Coachesx(name: "Veronica Loo", correo: "VeronicaEnriquez@correo.com", speciality: .noEngineer, photo: "https://ct.yimg.com/cy/4592/38222887850_6ad7b5_128sq.jpg"))
     }
     
-    private func updateView() {
-        
+    //tables
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return coachesArray.count
     }
     
-    //
-    private func setupSegmentedContro() {
-        segmentedControl.removeAllSegments()
-        segmentedControl.insertSegment(withTitle: "Coaches", at: 1, animated: false)
-        segmentedControl.insertSegment(withTitle: "Publications", at: 1, animated: false)
-        segmentedControl.addTarget(self, action: #selector(selectionDidChange(sender:)), for: .valueChanged)
-        segmentedControl.selectedSegmentIndex = 0
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        /*guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as? CoachTableViewController else {
+            return UITableViewCell()
+        }*/
+        return UITableViewCell()
     }
-    //Actions
-    @objc func selectionDidChange(sender: UISegmentedControl) {
-        updateView()
-    }*/
     
 }
+
+class Coachesx {
+    let name: String
+    let correo: String
+    let speciality: SpecialistType
+    let photo: String
+    
+    init(name: String,
+        correo: String,
+        speciality: SpecialistType,
+        photo: String) {
+        self.name = name
+        self.correo = correo
+        self.speciality = speciality
+        self.photo = photo
+    }
+}
+
+enum SpecialistType: String {
+    case engineer = "engineer"
+    case noEngineer = "noEngineer"
+}
+//https://www.b-plast2000.com/uploads/pics/maria_01.jpg
+//https://d26oc3sg82pgk3.cloudfront.net/files/media/edit/image/82/square_thumb%402x.jpg
+//https://www.ktbyte.com/resources/dist/assets/images/people/richard_zhou.jpg
+//https://ct.yimg.com/cy/4592/38222887850_6ad7b5_128sq.jpg
